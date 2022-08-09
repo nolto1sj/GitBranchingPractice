503820262f260b1ad2e67667382071c021f02585
nolto1sj <sjnolton@gmail.com>
2022-08-08 20:23:58 -0400

    added vehicle class and changed message

---
+++ Vehicle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitBranchingPractice
+{
+    public abstract class Vehicle
+    {
+        public string Name { get; set; }
+
+        public virtual void Go()
+        {
+            Console.WriteLine("Vehicle is going");
+        }
+    }
+}
